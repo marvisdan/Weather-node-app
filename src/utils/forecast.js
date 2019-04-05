b11e07e41b3844b81fcaefa622e695b7973a7759
Marvis Dan <marvis.dan@gmail.com>
2019-04-05 12:48:38 +0200

    add forcast date time

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,26 +1,25 @@
 
 const request = require('request');
-
+const moment = require('moment');
 const TOKEN_DARKSKY = '55fc15ddc27b49be3acd2e2b33d133fa';
 const forecast = (latitude, longitude, callback) => {
   const url = `https://api.darksky.net/forecast/${TOKEN_DARKSKY}/${latitude},${longitude}?units=si&lang=fr`;
-
   request({ url, json: true }, (error, { body }) => {
-
     if (error) {
       callback('Unable to connect to weather service', undefined);
     } else if (body.error) {
       callback('location not found', undefined);
     } else {
-      const { temperature, precipProbability } = body.currently;
+      const { temperature, precipProbability, time } = body.currently;
       let daily;
       let summary;
       if (body.daily.data && body.daily.data.length > 0) {
         daily = body.daily.data[0];
         summary = daily && daily.summary;
-        callback(undefined, `${summary} La température extérieure est de ${temperature} degrès actuelement. Il y a ${precipProbability}% chance de pleuvoir.`);
+        moment.locale('fr')
+        callback(undefined, `${summary}. Il est ${moment(time * 1000).format('HH:mm:ss')}. La température extérieure est de ${temperature} degrès actuellement. Il y a ${precipProbability}% chances de pleuvoir. ${summary}`);
       } else {
-        callback(undefined, `La température extérieure est de ${temperature} degrès actuelement. Il y a ${precipProbability}% chance de pleuvoir.`);
+        callback(undefined, `La température extérieure est de ${temperature} degrès actuellement. Il y a ${precipProbability}% chances de pleuvoir.`);
       }
     }
   });
